1010f3459ae92ac9d9863e3980c5f27ab63818c9
Sho0226 <s1F102201616@iniad.org>
2024-06-06 16:15:30 +0900

    add

---
+++ moocs/01:/add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    int y;
+
+    printf("X=");
+    scanf("%d",&x);
+
+    printf("Y=");
+    scanf("%d",&y);
+
+    printf("X + Y = %d + %d\n",x,y,x+y);
+
+    return 0;
+}
